449e88ab18ef3f5980580c85ab64134a0a2aacd4
Elder Flame Doron <adrianl87@nycstudents.net>
2025-09-25 14:13:52 +0000

    d

--- Lesson-07-activity/Main.java
+++ Lesson-07-activity/Main.java
@@ -9,11 +9,13 @@ class Main {
     Challenge 1:
     1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
 */
+
   System.out.println("Enter x value:");
   double x = Input.readDouble();
   double y = 0;
   y = Math.pow(x,7);
   System.out.println(y);
+
 /*  
     Challenge 2:
     1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
@@ -28,26 +30,40 @@ class Main {
     Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
     
 */
-
- 
-
+  System.out.println("Enter t value:");
+  double t = Input.readDouble();
+  System.out.println("Enter r value:");
+  double r = Input.readDouble();
+  double s = 0;
+  s = Math.pow(t,5)*Math.pow(r+2,4);
+  System.out.println(s);
 /*  
     Challenge 4:
     Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
-    
 */
-
-
-
+  System.out.println("Enter A value:");
+  double A = Input.readDouble();
+  System.out.println("Enter B value:");
+  double B = Input.readDouble();
+  double C = 0;
+  C = Math.sqrt(A+B);
+  System.out.println(C);
 /*  
     Challenge 5:
     Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
     
 */
-
-
-
-
+  System.out.println("Enter x1 value:");
+  double x1 = Input.readDouble();
+  System.out.println("Enter x2 value:");
+  double x2 = Input.readDouble();
+  System.out.println("Enter y1 value:");
+  double y1 = Input.readDouble();
+  System.out.println("Enter y2 value:");
+  double y2 = Input.readDouble();
+  double d = 0;
+  d = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+  System.out.println(d);
 /*  
     Challenge 6:
     Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
