21e1cb30bd870ccbcc28355750d80c394eed7071
Elder Flame Doron <adrianl87@nycstudents.net>
2025-09-30 14:08:40 +0000

    d

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -6,28 +6,33 @@ class Main {
 
   void init(){
 	print("hello");
-	FtoC(100);
-	sphereVolume(20);
-	coneVolume(2, 2);
-	distance(1, 2, 3, 4);
+	FtoC(100.0);
+	sphereVolume(20.0);
+	coneVolume(2.0, 2.0);
+	distance(1.0, 2.0, 3.0, 4.0);
   }
-  void print(String word){
+  String print(String word){
 	System.out.println(word);
+	return word;
   }
-  void FtoC(double F){
+  double FtoC(double F){
 	double C=(F-32)*(5/9.0);
 	System.out.println(C);
+	return C;
   }
-  void sphereVolume(double r){
+  double sphereVolume(double r){
 	double sV=(4/3.0)*Math.PI*Math.pow(r,3);
 	System.out.println(sV);
+	return sV;
   }
-  void coneVolume(double r2, double h){
+  double coneVolume(double r2, double h){
 	double cV=Math.PI*Math.pow(r2,2)*(h/3.0);
 	System.out.println(cV);
+	return cV;
   }
-  void distance(double x1, double x2, double y1, double y2){
+  double distance(double x1, double x2, double y1, double y2){
 	double slope=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 	System.out.println(slope);
+	return slope;
   }
 }
