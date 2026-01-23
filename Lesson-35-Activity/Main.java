b416fb7a1b62a343d127525b01a5944c25a1ceb4
Elder Flame Doron <adrianl87@nycstudents.net>
2026-01-23 03:00:37 +0000

    d

--- Lesson-35-Activity/Main.java
+++ Lesson-35-Activity/Main.java
@@ -13,24 +13,40 @@ class Main {
     // add your code below here
     double failing = 0;
     double passing = 0;
+    int failingcastro = 0;
+    int failingmusic = 0;
+    int freshiesophies = 0;
+    int banuporchetta = 0;
     for(int i = 0; i < students.length; i++){
       Student student = students[i];
+      if(student.gpa < 65 && student.findTeacher("castro r")){
+        failingcastro ++;
+      }
+      if(student.gpa < 65 && student.findCourseStartingWith("UL")){
+        failingmusic ++;
+      }
       if(student.gpa < 65){
         failing ++;
       }
       if(student.gpa >= 65){
         passing ++;
       }
+      if(student.gpa >= 65 && student.gradeLevel < 11){
+        freshiesophies ++;
+      }
+      if(student.teachers = student.findTeacher("banu") && student.teachers = student.findTeacher("porchetta")){
+        banuporchetta ++;
+      }
     }
-    failing = failing;
-    passing = passing;
     print(passing + " students are passing");
     print(failing + " students are failing");
     double percent = 0;
     percent = failing/students.length*100;
     print(percent + "% of students are failing");
-    
-    
+    print(failingcastro + " students are failing Mr. Castro");
+    print(failingmusic + " students are failing music");
+    print(freshiesophies + " freshmen and sophomores are passing");
+    print(banuporchetta + " students have both Banu and Porchetta");
   }
 
 
