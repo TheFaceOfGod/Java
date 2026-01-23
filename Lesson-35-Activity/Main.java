62c9a117f3e2ddd88b4420b397f97a0029a4cdf2
Elder Flame Doron <adrianl87@nycstudents.net>
2026-01-23 03:11:05 +0000

    d

--- Lesson-35-Activity/Main.java
+++ Lesson-35-Activity/Main.java
@@ -17,6 +17,11 @@ class Main {
     int failingmusic = 0;
     int freshiesophies = 0;
     int banuporchetta = 0;
+    int fsjs = 0;
+    int porchettafreshmen = 0;
+    int porchettasophomores = 0;
+    int porchettajuniors = 0;
+    int porchettaseniors = 0;
     for(int i = 0; i < students.length; i++){
       Student student = students[i];
       if(student.gpa < 65 && student.findTeacher("castro r")){
@@ -34,8 +39,23 @@ class Main {
       if(student.gpa >= 65 && student.gradeLevel < 11){
         freshiesophies ++;
       }
-      if(student.teachers = student.findTeacher("banu") && student.teachers = student.findTeacher("porchetta")){
+      if(student.findTeacher("banu") && student.findTeacher("porchetta")){
         banuporchetta ++;
+      }
+      if(student.gradeLevel > 8 && student.gradeLevel < 13){
+        fsjs ++;
+      }
+      if(student.findTeacher("porchetta") && student.gradeLevel == 9){
+        porchettafreshmen ++;
+      }
+      if(student.findTeacher("porchetta") && student.gradeLevel == 10){
+        porchettasophomores ++;
+      }
+      if(student.findTeacher("porchetta") && student.gradeLevel == 11){
+        porchettajuniors ++;
+      }
+      if(student.findTeacher("porchetta") && student.gradeLevel == 12){
+        porchettaseniors ++;
       }
     }
     print(passing + " students are passing");
@@ -47,6 +67,9 @@ class Main {
     print(failingmusic + " students are failing music");
     print(freshiesophies + " freshmen and sophomores are passing");
     print(banuporchetta + " students have both Banu and Porchetta");
+    print(fsjs + " students are freshmen and sophomores and juniors and seniors");
+    int studentnum = porchettafreshmen+porchettasophomores+porchettajuniors+porchettaseniors;
+    print("Out of " + studentnum + " students, " + porchettafreshmen + " are freshmen, " + porchettasophomores + " are sophomores, " + porchettajuniors + " are juniors, and " + porchettaseniors + " are seniors");
   }
 
 
