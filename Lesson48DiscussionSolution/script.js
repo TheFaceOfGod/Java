165428adf19535050e23f354b78f5523c454ffbb
Elder Flame Doron <adrianl87@nycstudents.net>
2026-04-22 13:38:52 +0000

    d

--- Lesson48DiscussionSolution/script.js
+++ Lesson48DiscussionSolution/script.js
@@ -2,7 +2,7 @@
 let data;
 
 async function init(){
-  let link ="https://friendly-journey-v6rvwqxgpp4pcpvqq-8500.app.github.dev/" ;
+  let link ="https://symmetrical-orbit-g4rq9pwrggjwcwpjv-8500.app.github.dev/" ;
   let route="customers"
   info = await fetch(link+route);
   data = await info.json();
---
+++ Lesson50ActivityWebsite/script.js
@@ -0,0 +1,28 @@
+let data,tracks;
+async function init(){
+  $.ajaxSetup({async: false});
+  let link = "https://symmetrical-orbit-g4rq9pwrggjwcwpjv-8500.app.github.dev";
+  let route= "/tracks"
+  info = await fetch(link+route);
+  tracks = await info.json();
+  console.log(tracks);
+
+  cards(tracks);
+}
+function cards(tracks){
+  let output = document.getElementById("output");
+  let build ="";
+  for(let i=0; i<tracks.length; i++){
+    let track = tracks[i];
+    build += `<div class="card" >`;
+    build += `<h3> Song Name </h3>`;
+    build += `<p> ${track.Name}</p>`;
+    build += `<div> Album </div>`;
+    build += `<div> ${track.Title} </div>`;
+    build += `<div> Composer </div>`;
+    build += `<div> ${track.Composer}</div>`;
+    build += `</div>`;
+  }
+  output.innerHTML = build;
+
+}
