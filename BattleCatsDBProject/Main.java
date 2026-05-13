e8020cb579c65f862f31c766da3ac9906a8ac789
Elder Flame Doron <adrianl87@nycstudents.net>
2026-05-13 13:58:04 +0000

    d

---
+++ BattleCatsDBProject/Main.java
@@ -0,0 +1,68 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.sql.*;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+//java -cp sqlite-jdbc-3.23.1.jar: Main
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; Main.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() throws IOException{
+   
+
+    // create a port - our Gateway
+    int port = 8300;
+      
+    //create the HTTPserver object
+    HttpServer server = HttpServer.create(new InetSocketAddress(port),0);
+
+    // create the database object
+    Database db = new Database("jdbc:sqlite:catTable.db");
+    Database db2 = new Database("jdbc:sqlite:enemyTable.db");
+    
+  
+    server.createContext("/", new RouteHandler("You are connected, but route not given or incorrect....") );
+
+    // Add your routes here....
+    String sql = "";
+    String queryResult ="";
+
+    String sql2 = "";
+    String queryResult2 ="";
+    server.createContext("/cats", new RouteHandler(db,sql) );
+    sql = "select * from catTable";
+    queryResult = db.runSQL(sql,"table-auto");
+    print(queryResult);
+    
+    server.createContext("/enemies", new RouteHandler(db2,sql2) );
+    sql2 = "select * from enemyTable";
+    queryResult2 = db2.runSQL(sql2,"table-auto");
+    print(queryResult2);
+      
+    //Start the server
+    server.start();
+
+    System.out.println("Server is listening on port "+port);
+       
+      
+    }    
+}
+
+
